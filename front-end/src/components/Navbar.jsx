b291ba04f62ef5fcf587c27c55ecae229c08e6c1
Virendra Kumar <vkprajapativiru.com>
2025-09-15 13:40:28 +0530

    logo fixed

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -13,14 +13,8 @@ const Navbar = () => {
       <div className="flex items-center justify-between whitespace-nowrap px-4 md:px-10 py-4 max-w-7xl mx-auto">
         <div className="flex items-center gap-10">
           <Link to="/" className="flex items-center gap-3 text-[var(--text-primary)] no-underline">
-            <svg className="h-8 w-8 text-[var(--primary-color)]" fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-              <g clipPath="url(#clip0_6_330)">
-                <path clipRule="evenodd" d="M24 0.757355L47.2426 24L24 47.2426L0.757355 24L24 0.757355ZM21 35.7574V12.2426L9.24264 24L21 35.7574Z" fill="currentColor" fillRule="evenodd"></path>
-              </g>
-              <defs>
-                <clipPath id="clip0_6_330"><rect fill="white" height="48" width="48"></rect></clipPath>
-              </defs>
-            </svg>
+            <img src="/cc_favicon.png" alt="CraftCurio Logo" className="h-8 w-8 object-contain" />
+
             <h2 className="text-2xl font-bold tracking-tight">CraftCurio</h2>
           </Link>
           <nav className="hidden md:flex items-center gap-8">
